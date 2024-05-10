f61613e41d3e43defe79c6be44f1c386acd95dd0
Nachiketa Vadera <nachiketa@posteo.net>
2024-05-10 14:41:08 +0530

    Initial Commit

---
+++ income-tax-sdk.xcframework/ios-arm64_x86_64-simulator/income_tax_sdk.framework/Headers/income_tax_sdk.h
@@ -0,0 +1,18 @@
+//
+//  income_tax_sdk.h
+//  income-tax-sdk
+//
+//  Created by Nachiketa Vadera on 06/03/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for income_tax_sdk.
+FOUNDATION_EXPORT double income_tax_sdkVersionNumber;
+
+//! Project version string for income_tax_sdk.
+FOUNDATION_EXPORT const unsigned char income_tax_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <income_tax_sdk/PublicHeader.h>
+
+
